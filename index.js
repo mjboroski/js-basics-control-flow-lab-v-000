e4b63cedaf3c7db1ab68388b20eab79c0e2e5b28
Michael Boroski <mjboroski@gmail.com>
2018-07-07 21:12:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 scuberGreetingForFeet(feet){
   switch{
     case feet <= 400:
-
+      'This one is on me!'
       break;
-    default:
-
+    case feet > 2000:
+      'I will gladly take your thirty bucks.'      
+      break;
+    case feet > 2500:
+      'No can do.'
+      break;
   }
+}
 
+ternaryCheckCity(city){
+  city === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
